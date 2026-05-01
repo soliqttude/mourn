f2942327ad8aadf360b3168cd1685754196657e9
jdubthemenace <azimsegashi@gmail.com>
2026-05-01 12:01:39 +0200

    Add src/commands/utility/avatar.ts

---
+++ src/commands/utility/avatar.ts
@@ -0,0 +1,25 @@
+import { ApplicationCommandOptionType } from "discord.js";
+import type { HybridCommand } from "../../lib/command.js";
+import { brandEmbed } from "../../lib/embeds.js";
+
+export const command: HybridCommand = {
+  name: "avatar",
+  aliases: ["av", "pfp"],
+  description: "Show a user's avatar.",
+  category: "utility",
+  options: [
+    { name: "user", description: "User", type: ApplicationCommandOptionType.User, required: false },
+  ],
+  async execute(ctx) {
+    const target = (await ctx.getUser("user")) ?? ctx.user;
+    return ctx.reply({
+      embeds: [
+        brandEmbed({
+          title: `${target.tag}'s avatar`,
+          image: target.displayAvatarURL({ size: 1024 }),
+          page: "Avatar",
+        }),
+      ],
+    });
+  },
+};
